1a2739a61c9fead6b92ef97e0ac746a1c86b37b0
Vaclav SRAIER <vasek@sraier.cz>
2015-06-20 22:04:34 +0200

    Added person removing and task adding
    How it works:
    	When a person is clicked, he gains one finished task
    	When a person is long-clicked, he is deleted
    
    Known issues:
    	Click on the plus sign crashes the app

--- app/src/main/java/cz/innovare/workometer/view/activities/StatusActivity.java
+++ app/src/main/java/cz/innovare/workometer/view/activities/StatusActivity.java
@@ -2,12 +2,16 @@ package cz.innovare.workometer.view.activities;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -15,6 +19,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.CursorAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -28,7 +33,7 @@ import cz.innovare.workometer.R;
 import cz.innovare.workometer.db.DbContract;
 import cz.innovare.workometer.db.DbHelper;
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,34 +51,11 @@ public class StatusActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
-
-
         ListView workers = (ListView) findViewById(R.id.workerListView);
-        //TextView empty = new TextView(this);
-        //empty.setText(R.string.empty_worker_listview);
-        //empty.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        //workers.setEmptyView(empty);
+        workers.setOnItemClickListener(this);
+        workers.setOnItemLongClickListener(this);
 
-        DbHelper dbHelper = new DbHelper(getApplicationContext());
-        SQLiteDatabase db = dbHelper.getReadableDatabase();
-        Cursor c = db.query(DbContract.Worker.TABLE_NAME,new String[]{DbContract.Worker._ID,DbContract.Worker.COLUMNN_NAME, DbContract.Worker.COLUMNN_FINISHED_GOALS},null,null,null,null, DbContract.Worker.COLUMNN_NAME);
-        int sumOfCompletedTasks = 0;
-        c.moveToFirst();
-        for(int x = 0; x < c.getCount(); x++) {
-            sumOfCompletedTasks = sumOfCompletedTasks + c.getInt(c.getColumnIndex(DbContract.Worker.COLUMNN_FINISHED_GOALS));
-            c.moveToNext();
-        }
-        c.close();
-
-        c = db.query(DbContract.Worker.TABLE_NAME,new String[]{DbContract.Worker._ID,DbContract.Worker.COLUMNN_NAME, DbContract.Worker.COLUMNN_FINISHED_GOALS},null,null,null,null, DbContract.Worker.COLUMNN_NAME);
-        if (c.getCount() != 0 || true) {
-            c.moveToFirst();
-            workers.setAdapter(new WorkersCursorAdapter(getApplicationContext(), c, PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getInt("taskGoal", 0),sumOfCompletedTasks));
-        }
-
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        setOverallProgress(sumOfCompletedTasks,sp.getInt("taskGoal",0));
-        setTotalTasks(sumOfCompletedTasks);
+        revalidate();
 
     }
 
@@ -86,8 +68,74 @@ public class StatusActivity extends Activity {
     protected void setOverallProgress(int sumOfTasks, int taskGoal) {
         DonutProgress prog = (DonutProgress)findViewById(R.id.overallProgress);
         prog.setMax(100);
-        prog.setProgress(Math.round(((float)sumOfTasks)/((float)taskGoal)*100));
+        prog.setProgress(Math.round(((float) sumOfTasks) / ((float) taskGoal) * 100));
     }
+
+
+    public void addNewUser(String name) {
+        ContentValues cv = new ContentValues();
+        cv.put(DbContract.Worker.COLUMNN_NAME,name);
+        cv.put(DbContract.Worker.COLUMNN_FINISHED_GOALS, 0);
+
+        DbHelper dbHelper = new DbHelper(getApplicationContext());
+        dbHelper.getWritableDatabase().insert(DbContract.Worker.TABLE_NAME, null, cv);
+    }
+
+    public void showNewUserDialog(View view) {
+        AlertDialog alertDialog = new AlertDialog.Builder(getApplicationContext())
+                .setView(getLayoutInflater().inflate(R.layout.fragment_dialog_add_worker,null,false))
+                .setTitle("Add new employee")
+                .setPositiveButton("Add",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                Log.i("AddUserDialog","Closed");
+                            }
+                        }
+                )
+                .setNegativeButton("Cancel", null)
+                .create();
+        alertDialog.show();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        DbHelper dbHelper = new DbHelper(getApplicationContext());
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        db.execSQL("UPDATE " + DbContract.Worker.TABLE_NAME + " SET " + DbContract.Worker.COLUMNN_FINISHED_GOALS + " = " + DbContract.Worker.COLUMNN_FINISHED_GOALS + " + 1 WHERE "+ DbContract.Worker._ID + " = "+ Long.toString(l));
+        db.close();
+        revalidate();
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+        DbHelper dbHelper = new DbHelper(getApplicationContext());
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        db.delete(DbContract.Worker.TABLE_NAME, DbContract.Worker._ID + " = ?", new String[]{Long.toString(l)});
+        db.close();
+        revalidate();
+        return false;
+    }
+
+    public void revalidate() {
+        ListView workers = (ListView) findViewById(R.id.workerListView);
+        DbHelper dbHelper = new DbHelper(getApplicationContext());
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor c = db.query(DbContract.Worker.TABLE_NAME,new String[]{DbContract.Worker._ID,DbContract.Worker.COLUMNN_NAME, DbContract.Worker.COLUMNN_FINISHED_GOALS},null,null,null,null, DbContract.Worker.COLUMNN_NAME);
+        int sumOfCompletedTasks = 0;
+        c.moveToFirst();
+        for(int x = 0; x < c.getCount(); x++) {
+            sumOfCompletedTasks = sumOfCompletedTasks + c.getInt(c.getColumnIndex(DbContract.Worker.COLUMNN_FINISHED_GOALS));
+            c.moveToNext();
+        }
+        c.moveToFirst();
+        workers.setAdapter(new WorkersCursorAdapter(getApplicationContext(), c, PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getInt("taskGoal", 0),sumOfCompletedTasks));
+
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        setOverallProgress(sumOfCompletedTasks,sp.getInt("taskGoal",0));
+        setTotalTasks(sumOfCompletedTasks);
+    }
+
+
 
 
     protected class WorkersCursorAdapter extends CursorAdapter {
@@ -119,5 +167,7 @@ public class StatusActivity extends Activity {
             bar.setProgress(tasks);
             title.setText(Math.round((float)tasks*100f/(float)sumOfFinishedTasks) + "%");
         }
+
+
     }
 }
